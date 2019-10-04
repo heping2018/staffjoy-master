2e19531b9bfa877b7b9b78cc27a0fe882a255ceb
heping <g246897@163.COM>
2019-10-04 22:48:03 +0800

    #1  配置-开发环境

--- commonlib/src/main/java/com/example/commonlib/commonlib/envconfig/EnvConfig.java
+++ commonlib/src/main/java/com/example/commonlib/commonlib/envconfig/EnvConfig.java
@@ -1,6 +1,8 @@
 package com.example.commonlib.commonlib.envconfig;
 
 
+import lombok.*;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,16 +11,28 @@ import java.util.Map;
  * 根据程序传入的参数确定使用哪个环境
  * 策略模式
  */
+
+@Data
+@Builder
 public class EnvConfig {
 
 
     private String name;
     private boolean debug;
-    private String ex
+    private String externalApex;
+    private String internalApex;
+    private String schame;
 
-
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
     private static Map<String,EnvConfig> map = new HashMap<String,EnvConfig>();
     static {
-
+        EnvConfig envConfig = EnvConfig.builder().name(EnvConstant.ENV_DEV)
+                .debug(true)
+                .externalApex("staffjoy.local")
+                .internalApex(EnvConstant.ENV_DEV)
+                .schame("http")
+                .build();
+        map.put(EnvConstant.ENV_DEV,envConfig);
     }
 }
